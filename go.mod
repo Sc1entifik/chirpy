b6b0e998126750f199e6c42647978d9f0edbf6f0
Scientifik <scientifik1@protonmail.com>
2026-09-02 16:16:46 -0400

    Added middleware for incrementing website hit counter. Also added reset route which resets counter. Used a struct with an atomic.Int32 to hold value and correctly keep count through go routines

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module chirpy
 
 go 1.26.6
+
+require golang.org/x/tools v0.49.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) returnRequests() int32 {
+
+	return cfg.fileserverHits.Load()
+}
+
+func (cfg *apiConfig) resetRequests() {
+	cfg.fileserverHits.Store(0)
+}
 
 
 func main() {
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
 	}
+	
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.returnRequests())))
+	}) 
+	
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(200)
+		apiCfg.resetRequests()
+		w.Write([]byte(fmt.Sprintf("Server Hits reset! %d", apiCfg.returnRequests())))
+	})
+
 	server.ListenAndServe()
 }
